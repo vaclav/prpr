66bb4e216aca7bf6f39a98654f85355026980501
Vaclav <vaclav.pech@gmail.com>
2019-05-30 17:51:48 +0200

    Zrychlovani

--- src/auticko/Ovladac.java
+++ src/auticko/Ovladac.java
@@ -6,7 +6,7 @@ import java.awt.event.KeyEvent;
 import static java.awt.event.KeyEvent.*;
 
 public class Ovladac extends KeyAdapter {
-  private final int krok = 5;
+  private int krok = 20;
   private Postava mojePostava;
   private final Hra hra;
 
@@ -21,19 +21,25 @@ public class Ovladac extends KeyAdapter {
     switch (keyCode) {
       case VK_UP:
         mojePostava.setY(mojePostava.getY() - krok);
-        mojePostava.setAktualniSmer(Smery.DOLU);
-        break;
-      case VK_DOWN:
-        mojePostava.setY(mojePostava.getY() - krok);
         mojePostava.setAktualniSmer(Smery.NAHORU);
         break;
+      case VK_DOWN:
+        mojePostava.setY(mojePostava.getY() + krok);
+        mojePostava.setAktualniSmer(Smery.DOLU);
+        break;
       case VK_LEFT:
-        mojePostava.setX(mojePostava.getX() + krok);
-        mojePostava.setAktualniSmer(Smery.DOPRAVA);
+        mojePostava.setX(mojePostava.getX() - krok);
+        mojePostava.setAktualniSmer(Smery.DOLEVA);
         break;
       case VK_RIGHT:
         mojePostava.setX(mojePostava.getX() + krok);
-        mojePostava.setAktualniSmer(Smery.DOLEVA);
+        mojePostava.setAktualniSmer(Smery.DOPRAVA);
+        break;
+      case VK_A:
+        krok = krok-5;
+        break;
+      case VK_Q:
+        krok = krok+5;
         break;
     }
     hra.repaint();
